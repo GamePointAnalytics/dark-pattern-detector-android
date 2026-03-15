3f70cb097eb0d10a0550277f55c356b0991a328c
GamePointAnalytics <Stephen.zhu6@gmail.com>
2026-03-15 15:52:48 -0400

    Update Gradle build config and add theme files

---
+++ app/src/main/java/com/safeweb/darkpatterndetector/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.safeweb.darkpatterndetector.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SafeGreen = Color(0xFF4CAF50)
+val WarningAmber = Color(0xFFFFBF00)
+val DangerRed = Color(0xFFD32F2F)
